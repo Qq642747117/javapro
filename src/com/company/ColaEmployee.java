3c8ada4d5efe806d310e947cbdb8cdd3fe35028f
HP <6427471172@qq.com>
2020-02-10 14:59:16 +0800

    Commit Message

---
+++ src/com/company/ColaEmployee.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public abstract class ColaEmployee {
+    public String name;
+    public int month;
+    public abstract double getSalary(int month);
+}
---
+++ src/com/company/SalariedEmployee.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class SalariedEmployee extends ColaEmployee{
+    public static int salar;
+    public SalariedEmployee(int salar){
+        this.salar=salar;
+    }
+    @Override
+    public double getSalary(int month) {
+        return month*salar;
+    }
+}
